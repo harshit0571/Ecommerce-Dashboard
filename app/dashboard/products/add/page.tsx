44fb5ecd43ad7a17030e75379fb2250836e9ca05
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-07-16 12:54:07 +0530

    product added to firebase

--- app/dashboard/products/add/page.tsx
+++ app/dashboard/products/add/page.tsx
@@ -1,28 +1,26 @@
 "use client";
 import React from "react";
-import { MdAddBox } from "react-icons/md";
-import { FaCheck } from "react-icons/fa";
 import ProductForm from "@/components/ProductForm";
+import { addDoc, collection, setDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
+  const addProduct = async (data: any) => {
+    try {
+      console.log(data);
+      await addDoc(collection(db, "Products"), data);
+      router.push("/dashboard/products");
+    } catch (error) {}
+  };
   return (
     <div className="w-full h-full">
-      <div className="w-full flex justify-between pt-10 px-10">
-        <div className="flex items-center gap-2">
-          <MdAddBox size={25} />
-          <p className="text-2xl ">Add New Product</p>
-        </div>
-        <div>
-          <button className="flex p-2 px-4 gap-2 items-center justify-center bg-green-300 rounded-full border-2 border-slate-700 hover:bg-white cursor-pointer">
-            <FaCheck size={15} />
-            <p> Add Product</p>
-          </button>
-        </div>
-      </div>
-
-      <ProductForm   productData={undefined} // pass this only when editing
-  onSubmit={()=>{}}
-/>
+      <ProductForm
+        productData={undefined} // pass this only when editing
+        onSubmit={addProduct}
+        type="add"
+      />
     </div>
   );
 };
--- components/ProductForm.tsx
+++ components/ProductForm.tsx
@@ -2,8 +2,8 @@
 import { db } from "@/firebase";
 import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { FaPlusCircle } from "react-icons/fa";
-import { MdCancel } from "react-icons/md";
+import { FaCheck, FaPlusCircle } from "react-icons/fa";
+import { MdAddBox, MdCancel } from "react-icons/md";
 
 interface Category {
   name: string;
@@ -22,11 +22,17 @@ interface ProductFormProps {
     category: string;
     subcategories: string[];
     sizes: string[];
+    listed?: boolean;
   };
   onSubmit: (data: any) => void;
+  type: string;
 }
 
-const ProductForm: React.FC<ProductFormProps> = ({ productData, onSubmit }) => {
+const ProductForm: React.FC<ProductFormProps> = ({
+  productData,
+  onSubmit,
+  type,
+}) => {
   const [showAddButton, setShowAddButton] = useState(false);
   const [ImagesStore, setImagesStore] = useState<string[]>(
     productData?.images || []
@@ -118,211 +124,235 @@ const ProductForm: React.FC<ProductFormProps> = ({ productData, onSubmit }) => {
       category: selectedParentCategory,
       subcategories: selectedSubcategories,
       sizes: selectedSizes,
+      listed: true,
     };
+    onSubmit(formData);
     console.log(formData, "form");
   };
 
   return (
-    <div className="flex px-10 mt-8 gap-5 pb-5">
-      <div className="w-[70%] rounded-lg flex h-max flex-col gap-5">
-        <div className="flex-col p-3 gap-5 bg-neutral-100 rounded-lg flex">
-          <h1 className="text-lg font-semibold" onClick={()=>{handleSubmit()}}>General Information</h1>
-          <div className="flex flex-col gap-1">
-            <p>Name Product</p>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="bg-neutral-200 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <p>Description of Product</p>
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="bg-neutral-200 p-2 min-h-[100px] rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
-            />
-          </div>
-          <div className="flex flex-col">
-            <h1>Size</h1>
-            <p className="text-gray-400 text-sm">Pick available sizes</p>
-            <div className="flex gap-2 mt-2">
-              {["XS", "S", "M", "L", "XL", "XXL"].map((size) => (
-                <div
-                  key={size}
-                  className={`p-2 bg-neutral-300 rounded-md cursor-pointer hover:bg-green-200 ${
-                    selectedSizes.includes(size) ? "bg-green-200" : ""
-                  }`}
-                  onClick={() => handleSizeClick(size)}
-                >
-                  {size}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="flex flex-col gap-1">
-            <h1>Gender</h1>
-            <div className="flex gap-2 mt-1">
-              {["Male", "Female", "Unisex"].map((g) => (
-                <div key={g} className="flex items-center gap-1">
-                  <input
-                    type="radio"
-                    id={g}
-                    name="gender"
-                    value={g}
-                    checked={gender === g}
-                    onChange={(e) => setGender(e.target.value)}
-                    className="cursor-pointer"
-                  />
-                  <label htmlFor={g} className="cursor-pointer">
-                    {g}
-                  </label>
-                </div>
-              ))}
-            </div>
-          </div>
+    <div>
+      <div className="w-full flex justify-between pt-10 px-10">
+        <div className="flex items-center gap-2">
+          <MdAddBox size={25} />
+          <p className="text-2xl ">
+            {type === "edit" ? "Edit Product" : "Add New Product"}
+          </p>
         </div>
-        <div className="flex flex-col bg-neutral-100 p-3 rounded-lg gap-5">
-          <h1 className="text-lg font-semibold">Pricing And Stock</h1>
-          <div className="flex flex-col gap-3">
-            <div className="flex gap-4">
-              <div className="flex flex-col flex-grow gap-1">
-                <p>Price</p>
-                <input
-                  type="number"
-                  value={price}
-                  onChange={(e) => setPrice(Number(e.target.value))}
-                  className="bg-neutral-200 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
-                  min={0}
-                />
-              </div>
-              <div className="flex flex-col flex-grow gap-1">
-                <p>Discount</p>
-                <input
-                  type="number"
-                  value={discount}
-                  onChange={(e) => setDiscount(Number(e.target.value))}
-                  min={0}
-                  className="bg-neutral-200 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
-                />
+        <div>
+          <button
+            className="flex p-2 px-4 gap-2 items-center justify-center bg-green-300 rounded-full border-2 border-slate-700 hover:bg-white cursor-pointer"
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            <FaCheck size={15} />
+            <p> {type === "edit" ? "Edit Product" : "Add Product"}</p>
+          </button>
+        </div>
+      </div>
+      <div className="flex px-10 mt-8 gap-5 pb-5">
+        <div className="w-[70%] rounded-lg flex h-max flex-col gap-5">
+          <div className="flex-col p-3 gap-5 bg-neutral-100 rounded-lg flex">
+            <h1 className="text-lg font-semibold">General Information</h1>
+            <div className="flex flex-col gap-1">
+              <p>Name Product</p>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="bg-neutral-200 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <p>Description of Product</p>
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="bg-neutral-200 p-2 min-h-[100px] rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+              />
+            </div>
+            <div className="flex flex-col">
+              <h1>Size</h1>
+              <p className="text-gray-400 text-sm">Pick available sizes</p>
+              <div className="flex gap-2 mt-2">
+                {["XS", "S", "M", "L", "XL", "XXL"].map((size) => (
+                  <div
+                    key={size}
+                    className={`p-2 bg-neutral-300 rounded-md cursor-pointer hover:bg-green-200 ${
+                      selectedSizes.includes(size) ? "bg-green-200" : ""
+                    }`}
+                    onClick={() => handleSizeClick(size)}
+                  >
+                    {size}
+                  </div>
+                ))}
               </div>
             </div>
             <div className="flex flex-col gap-1">
-              <h2>Stock</h2>
-              {selectedSizes.map((size) => (
-                <div key={size} className="flex items-center gap-2">
-                  <p className="w-12">{size}</p>
+              <h1>Gender</h1>
+              <div className="flex gap-2 mt-1">
+                {["Male", "Female", "Unisex"].map((g) => (
+                  <div key={g} className="flex items-center gap-1">
+                    <input
+                      type="radio"
+                      id={g}
+                      name="gender"
+                      value={g}
+                      checked={gender === g}
+                      onChange={(e) => setGender(e.target.value)}
+                      className="cursor-pointer"
+                    />
+                    <label htmlFor={g} className="cursor-pointer">
+                      {g}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col bg-neutral-100 p-3 rounded-lg gap-5">
+            <h1 className="text-lg font-semibold">Pricing And Stock</h1>
+            <div className="flex flex-col gap-3">
+              <div className="flex gap-4">
+                <div className="flex flex-col flex-grow gap-1">
+                  <p>Price</p>
                   <input
                     type="number"
-                    value={stock[size] || ""}
-                    onChange={(e) =>
-                      handleStockChange(size, Number(e.target.value))
-                    }
-                    className="bg-neutral-200 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black flex-grow"
-                    placeholder={`Stock for ${size}`}
+                    value={price}
+                    onChange={(e) => setPrice(Number(e.target.value))}
+                    className="bg-neutral-200 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
                     min={0}
                   />
                 </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="w-[30%] h-max rounded-lg flex flex-col gap-5">
-        <div className="bg-neutral-100 rounded-lg flex flex-col w-full p-3 gap-5">
-          <h1 className="text-lg font-semibold">Upload Images</h1>
-          <div className="flex flex-col gap-3 relative">
-            {showAddButton && (
-              <div className="absolute flex flex-col z-50 justify-center items-center bg-neutral-300 p-3 rounded-xl bottom-0 w-full transition-all duration-150">
-                <input
-                  type="text"
-                  className="bg-white w-full p-2 ring-black ring-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
-                  placeholder="add image url"
-                  value={imageUrlVal}
-                  onChange={(e) => {
-                    setImageUrlVal(e.target.value);
-                  }}
-                />
-                <button
-                  className="p-2 mt-3 w-full bg-slate-700 text-white font-semibold h-max rounded-full "
-                  onClick={() => handleImagesAdd(imageUrlVal)}
-                >
-                  Add image
-                </button>
-                <button
-                  className=" w-max h-max rounded-full font-semibold text-red-500 "
-                  onClick={() => {
-                    setShowAddButton(false);
-                  }}
-                >
-                  Cancel
-                </button>
-              </div>
-            )}
-            <div className="w-full h-[250px] bg-neutral-200 rounded-md">
-              <img
-                src={ImagesStore.length > 0 ? ImagesStore[displayImage] : ""}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="flex gap-2 overflow-auto w-full">
-              {ImagesStore.map((image, index) => (
-                <div
-                  key={index}
-                  className={`bg-neutral-100 h-[50px] w-[50px] rounded-md flex justify-center items-center hover:bg-neutral-300 cursor-pointer ${
-                    index === displayImage ? "border-black border-2" : ""
-                  }`}
-                  onClick={() => setDisplayImage(index)}
-                >
-                  <img src={image} className="w-full h-full object-cover" />
-                </div>
-              ))}
-              <div
-                className="bg-neutral-100 h-[50px] w-[50px] rounded-md flex justify-center items-center hover:bg-neutral-300 cursor-pointer border-dotted border-2 border-slate-400"
-                onClick={() => setShowAddButton(true)}
-              >
-                <FaPlusCircle color="light-green" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="h-max w-full bg-neutral-100 rounded-lg flex flex-col p-3 gap-1">
-          <h1 className="text-lg font-semibold">Categories</h1>
-          <p className="text-gray-400">Choose parent category</p>
-          <select
-            className="bg-neutral-200 p-2 rounded-md"
-            value={selectedParentCategory}
-            onChange={(e) => setSelectedParentCategory(e.target.value)}
-          >
-            <option disabled value="">
-              Please select a category
-            </option>
-            {categories.length > 0 &&
-              categories.map((category) => (
-                <option key={category.name} value={category.name}>
-                  {category.name}
-                </option>
-              ))}
-          </select>
-          {selectedParentCategory !== "" &&
-            categories
-              .find((cat) => cat.name === selectedParentCategory)
-              ?.subcategories.map((sub) => (
-                <div key={sub} className="flex items-center gap-1">
+                <div className="flex flex-col flex-grow gap-1">
+                  <p>Discount</p>
                   <input
-                    type="checkbox"
-                    id={sub}
-                    value={sub}
-                    checked={selectedSubcategories.includes(sub)}
-                    onChange={() => handleSubcategoryChange(sub)}
-                    className="cursor-pointer"
+                    type="number"
+                    value={discount}
+                    onChange={(e) => setDiscount(Number(e.target.value))}
+                    min={0}
+                    className="bg-neutral-200 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
                   />
-                  <label htmlFor={sub} className="cursor-pointer">
-                    {sub}
-                  </label>
                 </div>
-              ))}
+              </div>
+              <div className="flex flex-col gap-1">
+                <h2>Stock</h2>
+                {selectedSizes.map((size) => (
+                  <div key={size} className="flex items-center gap-2">
+                    <p className="w-12">{size}</p>
+                    <input
+                      type="number"
+                      value={stock[size] || ""}
+                      onChange={(e) =>
+                        handleStockChange(size, Number(e.target.value))
+                      }
+                      className="bg-neutral-200 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black flex-grow"
+                      placeholder={`Stock for ${size}`}
+                      min={0}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[30%] h-max rounded-lg flex flex-col gap-5">
+          <div className="bg-neutral-100 rounded-lg flex flex-col w-full p-3 gap-5">
+            <h1 className="text-lg font-semibold">Upload Images</h1>
+            <div className="flex flex-col gap-3 relative">
+              {showAddButton && (
+                <div className="absolute flex flex-col z-50 justify-center items-center bg-neutral-300 p-3 rounded-xl bottom-0 w-full transition-all duration-150">
+                  <input
+                    type="text"
+                    className="bg-white w-full p-2 ring-black ring-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+                    placeholder="add image url"
+                    value={imageUrlVal}
+                    onChange={(e) => {
+                      setImageUrlVal(e.target.value);
+                    }}
+                  />
+                  <button
+                    className="p-2 mt-3 w-full bg-slate-700 text-white font-semibold h-max rounded-full "
+                    onClick={() => handleImagesAdd(imageUrlVal)}
+                  >
+                    Add image
+                  </button>
+                  <button
+                    className=" w-max h-max rounded-full font-semibold text-red-500 "
+                    onClick={() => {
+                      setShowAddButton(false);
+                    }}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              )}
+              <div className="w-full h-[250px] bg-neutral-200 rounded-md">
+                <img
+                  src={ImagesStore.length > 0 ? ImagesStore[displayImage] : ""}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex gap-2 overflow-auto w-full">
+                {ImagesStore.map((image, index) => (
+                  <div
+                    key={index}
+                    className={`bg-neutral-100 h-[50px] w-[50px] rounded-md flex justify-center items-center hover:bg-neutral-300 cursor-pointer ${
+                      index === displayImage ? "border-black border-2" : ""
+                    }`}
+                    onClick={() => setDisplayImage(index)}
+                  >
+                    <img src={image} className="w-full h-full object-cover" />
+                  </div>
+                ))}
+                <div
+                  className="bg-neutral-100 h-[50px] w-[50px] rounded-md flex justify-center items-center hover:bg-neutral-300 cursor-pointer border-dotted border-2 border-slate-400"
+                  onClick={() => setShowAddButton(true)}
+                >
+                  <FaPlusCircle color="light-green" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="h-max w-full bg-neutral-100 rounded-lg flex flex-col p-3 gap-1">
+            <h1 className="text-lg font-semibold">Categories</h1>
+            <p className="text-gray-400">Choose parent category</p>
+            <select
+              className="bg-neutral-200 p-2 rounded-md"
+              value={selectedParentCategory}
+              onChange={(e) => setSelectedParentCategory(e.target.value)}
+            >
+              <option disabled value="">
+                Please select a category
+              </option>
+              {categories.length > 0 &&
+                categories.map((category) => (
+                  <option key={category.name} value={category.name}>
+                    {category.name}
+                  </option>
+                ))}
+            </select>
+            {selectedParentCategory !== "" &&
+              categories
+                .find((cat) => cat.name === selectedParentCategory)
+                ?.subcategories.map((sub) => (
+                  <div key={sub} className="flex items-center gap-1">
+                    <input
+                      type="checkbox"
+                      id={sub}
+                      value={sub}
+                      checked={selectedSubcategories.includes(sub)}
+                      onChange={() => handleSubcategoryChange(sub)}
+                      className="cursor-pointer"
+                    />
+                    <label htmlFor={sub} className="cursor-pointer">
+                      {sub}
+                    </label>
+                  </div>
+                ))}
+          </div>
         </div>
       </div>
     </div>
